7c13431f210bc503e4ecbbe090d18096a1e300bc
luo hao <990747088@qq.com>
2018-04-20 17:11:52 +0800

    add utils

--- utils/file/path.go
+++ utils/file/path.go
@@ -2,14 +2,11 @@ package file
 
 import (
 	"os"
-
-	"github.com/sirupsen/logrus"
 )
 
 func PathExists(path string) (exists bool) {
 	_, err := os.Stat(path)
 	if nil != err {
-		logrus.Errorf("path: %p not exists. \n%s.", path, err)
 		return
 	}
 

3b5a695562a909ff142a0491baa753fedc49b9dd
luo hao <990747088@qq.com>
2018-05-17 13:08:01 +0800

    add db client

--- db/client.go
+++ db/client.go
@@ -1,34 +1,66 @@
 package db
 
 import (
-	"github.com/jmoiron/sqlx"
 	"context"
-	"github.com/sirupsen/logrus"
 	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
 )
 
 type ConnectionChecker interface {
-	Before(client *Client) error
-	After(err error) error
+	Before(client *Client)
+	After(err error)
+}
+
+type ConnectionCheckerMaker interface {
+	NewChecker() ConnectionChecker
 }
 
 type CommandChecker interface {
-	Before(strSql string, args ...interface{}) error
-	After(err error) error
+	Before(strSql string, args ...interface{})
+	After(err error)
+}
+
+type CommandCheckerMaker interface {
+	NewChecker() CommandChecker
 }
 
 type Client struct {
-	DB *sqlx.DB
-	Ctx context.Context
-	ConnectionCheckers []ConnectionChecker
-	CommandCheckers []CommandChecker
+	DB                     *sqlx.DB
+	Ctx                    context.Context
+	ConnectionCheckerMaker ConnectionCheckerMaker
+	CommandCheckerMaker    CommandCheckerMaker
 }
 
-func NewClient(ctx context.Context, sqlxDB * sqlx.DB) (client *Client, err error) {
-	client = &Client{
-		DB: sqlxDB,
-		Ctx:ctx,
+func (m *Client) ConnectionChecker() ConnectionChecker {
+	if m.ConnectionCheckerMaker == nil {
+		return nil
 	}
+	return m.ConnectionCheckerMaker.NewChecker()
+}
+
+func (m *Client) CommandChecker() CommandChecker {
+	if m.CommandCheckerMaker == nil {
+		return nil
+	}
+
+	return m.CommandCheckerMaker.NewChecker()
+}
+
+func NewClient(ctx context.Context, sqlxDB *sqlx.DB) (client *Client, err error) {
+	client = &Client{
+		DB:  sqlxDB,
+		Ctx: ctx,
+	}
+
+	// checker
+	checker := client.ConnectionChecker()
+	if checker != nil {
+		checker.Before(client)
+		defer checker.After(err)
+	}
+
 	err = client.DB.PingContext(ctx)
 	if nil != err {
 		logrus.Errorf("ping db failed. %s", err)
@@ -37,32 +69,82 @@ func NewClient(ctx context.Context, sqlxDB * sqlx.DB) (client *Client, err error
 	return
 }
 
-func (m *Client) QueryRowx(query string, args ...interface{}) (*sqlx.Row, error) {
-	return m.DB.QueryRowxContext(m.Ctx, query, args...), nil
+func (m *Client) QueryRowx(query string, args ...interface{}) (rows *sqlx.Row, err error) {
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, args...)
+		defer checker.After(err)
+	}
+
+	rows = m.DB.QueryRowxContext(m.Ctx, query, args...)
+	return
 }
 
 func (m *Client) Get(dest interface{}, query string, args ...interface{}) (err error) {
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, args...)
+		defer checker.After(err)
+	}
+
 	err = m.DB.GetContext(m.Ctx, dest, query, args...)
 	return
 }
 
 func (m *Client) NamedQuery(query string, arg interface{}) (rows *sqlx.Rows, err error) {
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, arg)
+		defer checker.After(err)
+	}
 	rows, err = m.DB.NamedQueryContext(m.Ctx, query, arg)
 	return
 }
 
-func (m *Client) Query(query string, args ...interface{})(*sqlx.Rows, error) {
-	return m.DB.QueryxContext(m.Ctx, query, args...)
+func (m *Client) Query(query string, args ...interface{}) (rows *sqlx.Rows, err error) {
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, args...)
+		defer checker.After(err)
+	}
+
+	rows, err = m.DB.QueryxContext(m.Ctx, query, args...)
+	return
 }
 
-func (m *Client) Exec(query string, args ...interface{}) (sql.Result, error){
-	return m.DB.ExecContext(m.Ctx, query, args...)
+func (m *Client) Exec(query string, args ...interface{}) (result sql.Result, err error) {
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, args...)
+		defer checker.After(err)
+	}
+	result, err = m.DB.ExecContext(m.Ctx, query, args...)
+	return
 }
 
 func (m *Client) NamedExec(query string, arg interface{}) (result sql.Result, err error) {
-	return m.DB.NamedExecContext(m.Ctx, query, arg)
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, arg)
+		defer checker.After(err)
+	}
+	result, err = m.DB.NamedExecContext(m.Ctx, query, arg)
+	return
 }
 
 func (m *Client) Select(slice interface{}, query string, args ...interface{}) (err error) {
-	return m.DB.SelectContext(m.Ctx, slice, query, args...)
+	// checker
+	checker := m.CommandChecker()
+	if checker != nil {
+		checker.Before(query, args...)
+		defer checker.After(err)
+	}
+	err = m.DB.SelectContext(m.Ctx, slice, query, args...)
+	return
 }

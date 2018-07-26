472f31eb12de39bb251758847808512257e1e838
luo hao <990747088@qq.com>
2018-07-26 18:02:39 +0800

    add web image type

--- utils/uimage/image.go
+++ utils/uimage/image.go
@@ -1,23 +1,36 @@
 package uimage
 
 import (
-	"errors"
 	"image"
 	"image/gif"
 	"image/jpeg"
 	"image/png"
+
 	"io"
+
+	"github.com/sirupsen/logrus"
+	_ "golang.org/x/image/webp" // webp只有decode，没有encode
+
+	"golang.org/x/image/bmp"
 )
 
 const TYPE_JPEG = "jpeg"
 const TYPE_PNG = "png"
 const TYPE_GIF = "gif"
+const TYPE_BMP = "bmp"
 
-func Decode(r io.Reader) (image.Image, string, error) {
-	return image.Decode(r)
+func Decode(r io.Reader) (img image.Image, imgType string, err error) {
+	img, imgType, err = image.Decode(r)
+	if nil != err {
+		logrus.Errorf("decode image failed. %s.", err)
+		return
+	}
+	return
 }
 
-func Encode(imgType string, writer io.Writer, img image.Image) (err error) {
+// encode后可能会改变图片类型
+func Encode(imgType string, writer io.Writer, img image.Image) (newImgType string, err error) {
+	newImgType = imgType
 	switch imgType {
 	case TYPE_JPEG:
 		err = jpeg.Encode(writer, img, nil)
@@ -25,8 +38,12 @@ func Encode(imgType string, writer io.Writer, img image.Image) (err error) {
 		err = png.Encode(writer, img)
 	case TYPE_GIF:
 		err = gif.Encode(writer, img, nil)
+	case TYPE_BMP:
+		err = bmp.Encode(writer, img)
 	default:
-		err = errors.New("unknown image type")
+		newImgType = TYPE_PNG
+		logrus.Warnf("encode unsupported encode image type: %s to png", imgType)
+		err = png.Encode(writer, img) // 转成png
 	}
 	return
 }

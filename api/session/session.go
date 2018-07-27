7ad498802def96d3057e0566b8768b40d848ae7d
luo hao <990747088@qq.com>
2018-07-27 14:36:49 +0800

    modify session

--- api/session/session.go
+++ api/session/session.go
@@ -1,11 +1,14 @@
 package session
 
 import (
-	"github.com/haozzzzzzzz/go-rapid-development/api/code"
 	"time"
+
+	"github.com/haozzzzzzzz/go-rapid-development/api/code"
+	"github.com/haozzzzzzzz/go-rapid-development/web/ginbuilder"
 )
 
 type AppSession struct {
+	Ctx         *ginbuilder.Context
 	RequestData struct {
 		AppVersion     string `json:"app_version"`
 		AppVersionCode uint32 `json:"app_version_code"`
@@ -24,6 +27,7 @@ type AppSession struct {
 }
 
 type ManageSession struct {
+	Ctx         *ginbuilder.Context
 	RequestData struct {
 		UserId   string `json:"user_id" form:"user_id" binding:"required"`   // 用户ID
 		UserName string `json:"username" form:"username" binding:"required"` // 用户名

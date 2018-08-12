69beb354346d5352d42586a2d1b4f88109f99f0b
luo hao <990747088@qq.com>
2018-08-12 14:23:41 +0800

    modify handler

--- web/ginbuilder/handler.go
+++ web/ginbuilder/handler.go
@@ -1,7 +1,11 @@
 package ginbuilder
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/haozzzzzzzz/go-rapid-development/utils/uerrors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -24,11 +28,32 @@ func (m *HandleFunc) GinHandler(ginCtx *gin.Context) {
 		defer func() {
 			var errPanic interface{}
 			if errPanic = recover(); errPanic != nil {
-				logrus.Panic(errPanic)
 				ctx.Session.Panic(errPanic)
+				if err == nil {
+					err = uerrors.Newf("panic occurs: %#v", errPanic)
+				}
 			}
+			defer func() { // 先处理，后抛出
+				if errPanic != nil {
+					logrus.Panic(errPanic)
+				}
+			}()
 
 			ctx.Session.AfterHandle(err)
+
+			// print error
+			if err != nil {
+				ctx.Logger.Errorf("error occurs: %#v", err)
+
+				if ctx.ResponseData != nil {
+					byteResponseData, errMarshal := json.Marshal(ctx.ResponseData)
+					if errMarshal != nil {
+						log.Printf("marshal response data failed.\n%#v\n", ctx.ResponseData)
+					}
+
+					log.Println(string(byteResponseData))
+				}
+			}
 
 		}()
 

5724471ed52f7adc3d0ac11b8c7335ca580eb64f
luo hao <990747088@qq.com>
2019-03-01 19:35:14 +0800

    add db error func

--- db/error.go
+++ db/error.go
@@ -1,3 +1,32 @@
 package db
 
+import (
+	"github.com/go-sql-driver/mysql"
+)
+
 const ErrorDuplicateEntryForKey = 1062
+const ErrorTableAlreadyExists = 1050
+
+func IsErrorDuplicateEntryForKey(err error) (result bool) {
+	mysqlError, ok := err.(*mysql.MySQLError)
+	if !ok {
+		return
+	} else {
+		if mysqlError.Number == ErrorDuplicateEntryForKey {
+			result = true
+		}
+	}
+	return
+}
+
+func IsErrorTableAlreadyExists(err error) (result bool) {
+	mysqlError, ok := err.(*mysql.MySQLError)
+	if !ok {
+		return
+	} else {
+		if mysqlError.Number == ErrorTableAlreadyExists {
+			result = true
+		}
+	}
+	return
+}

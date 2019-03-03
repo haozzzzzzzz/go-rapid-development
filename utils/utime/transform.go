3ec3999bfe8f8f57135e9e4a8432134b6f32cbfe
luo hao <990747088@qq.com>
2019-03-04 00:12:08 +0800

    modify

--- utils/utime/transform.go
+++ utils/utime/transform.go
@@ -4,9 +4,6 @@ import "time"
 
 // 一天的开始时间
 func DayStartTime(t time.Time) (dayStartTime time.Time) {
-	//strDayStartTime := t.Format("2006-01-02 00:00:00 -0700 MST")
-	//dayStartTime, err = time.Parse("2006-01-02 15:04:05 -0700 MST", strDayStartTime)
-	//
 	year, month, day := t.Date()
 	dayStartTime = time.Date(year, month, day, 0, 0, 0, 0, t.Location())
 	return
@@ -35,7 +32,7 @@ func WeekStartTime(t time.Time) (weekStartTime time.Time) {
 	var secondsPerDay int64 = 60 * 60 * 24
 	offset := int64(int(weekDay)-1) * secondsPerDay
 	weekStartUnix = dayStartUnix - offset
-	weekStartTime = time.Unix(weekStartUnix, 0)
+	weekStartTime = time.Unix(weekStartUnix, 0).In(t.Location())
 
 	if weekDay == 0 { // 周日属于上一周
 		weekStartTime = OffsetWeekStartTime(weekStartTime, -1)

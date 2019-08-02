b09857d496732a3941cd3e4645a66c0633a12b5a
luo hao <990747088@qq.com>
2019-08-02 20:31:12 +0800

    add error return

--- tools/code/cmd/init.go
+++ tools/code/cmd/init.go
@@ -9,5 +9,5 @@ var RootCmd = rootCmd
 
 func init() {
 	RootCmd.AddCommand(precompile.CommandPrecompile())
-	RootCmd.AddCommand(ertn.CommandErtn())
+	RootCmd.AddCommand(ertn.CommandErrorReturn())
 }

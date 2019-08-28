6d47b59eaa8131d17998ab454198a0b0a874005d
luo hao <990747088@qq.com>
2019-08-28 12:38:12 +0800

    modifyg

--- utils/uctx/ctx.go
+++ utils/uctx/ctx.go
@@ -2,8 +2,13 @@ package uctx
 
 import (
 	"context"
+	"time"
 )
 
 func BackgroundWithCancel() (ctx context.Context, cancelFunc context.CancelFunc) {
 	return context.WithCancel(context.Background())
+}
+
+func BackgroundWithTimeout(timeout time.Duration) (ctx context.Context, cancelFunc context.CancelFunc) {
+	return context.WithTimeout(context.Background(), timeout)
 }

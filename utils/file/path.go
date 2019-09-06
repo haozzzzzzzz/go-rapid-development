6889789e55a38dc3df2e06e2bad4d12e4d34ac83
luo hao <990747088@qq.com>
2019-09-06 14:34:03 +0800

    modify

--- utils/file/path.go
+++ utils/file/path.go
@@ -1,7 +1,10 @@
 package file
 
 import (
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/haozzzzzzzz/go-rapid-development/utils/str"
@@ -19,4 +22,27 @@ func PathExists(path string) (exists bool) {
 
 func ParentDir(dir string) string {
 	return str.SubString(dir, 0, strings.LastIndex(dir, "/"))
+}
+
+// if file witch specified extension exists in directory
+func DirHasExtFile(dir string, ext string) (hasExt bool, err error) {
+	files, err := ioutil.ReadDir(dir)
+	if nil != err {
+		logrus.Errorf("read directory %s failed. error: %s.", dir, err)
+		return
+	}
+
+	for _, fileInfo := range files {
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		fileExt := filepath.Ext(fileInfo.Name())
+		if fileExt == ext {
+			hasExt = true
+			break
+		}
+	}
+
+	return
 }
--- utils/file/path_test.go
+++ utils/file/path_test.go
@@ -8,3 +8,13 @@ import (
 func TestParentDir(t *testing.T) {
 	fmt.Println(ParentDir("/"))
 }
+
+func TestDirHasExtFile(t *testing.T) {
+	hasExt, err := DirHasExtFile("/Users/hao/Documents/Projects/Github/go-rapid-development/utils/file", ".go")
+	if nil != err {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(hasExt)
+}

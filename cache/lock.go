da9badd01dda6c69434bbbaf8e05e08ca43ddaf6
luo hao <990747088@qq.com>
2019-12-10 14:39:33 +0800

    modify

--- cache/lock.go
+++ cache/lock.go
@@ -24,11 +24,22 @@ func (m *RedisLock) KeyLock(key string) string {
 	return fmt.Sprintf("LOCK:%s", key)
 }
 
-func (m *RedisLock) Lock(key string, wait bool) (err error) {
+// wait lock
+func (m *RedisLock) Lock(
+	key string,
+	wait bool,
+	waitDur time.Duration,
+	expire time.Duration,
+) (err error) {
 	key = m.KeyLock(key)
 	var ok bool
+
+	if expire == 0 {
+		expire = TTL_REDIS_LOCK
+	}
+
 	for i := 0; i < 100; i++ {
-		ok, err = m.Client.SetNX(key, "LOCKED", TTL_REDIS_LOCK) // 尝试上锁
+		ok, err = m.Client.SetNX(key, "LOCKED", expire) // 尝试上锁
 		if nil != err {
 			logrus.Errorf("set redis lock failed. %s.", err)
 			break
@@ -38,6 +49,9 @@ func (m *RedisLock) Lock(key string, wait bool) (err error) {
 			break
 		}
 
+		if wait && waitDur > 0 {
+			time.Sleep(waitDur)
+		}
 	}
 
 	if !ok {
@@ -52,6 +66,29 @@ func (m *RedisLock) Unlock(key string) (err error) {
 	_, err = m.Client.Del(key)
 	if nil != err {
 		logrus.Errorf("del redis lock failed. %s.", err)
+		return
+	}
+	return
+}
+
+// not wait
+func (m *RedisLock) LockNotWaitLock(key string, expire time.Duration) (success bool) {
+	if expire == 0 {
+		expire = TTL_REDIS_LOCK
+	}
+
+	success, err := m.Client.SetNX(key, "LOCKED", expire)
+	if nil != err {
+		logrus.Errorf("set not wait lock failed. error: %s.", err)
+		return
+	}
+	return
+}
+
+func (m *RedisLock) UnlockNotWaitLock(key string) (success bool) {
+	_, err := m.Client.Del(key)
+	if nil != err {
+		logrus.Errorf("unlock not wait lock failed. error: %s.", err)
 		return
 	}
 	return

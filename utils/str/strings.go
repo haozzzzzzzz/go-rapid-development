a56825a19a043c3eedce2d1c2b2460d158337ba5
luo hao <990747088@qq.com>
2019-12-11 11:11:22 +0800

    modify

--- utils/str/strings.go
+++ utils/str/strings.go
@@ -8,3 +8,34 @@ func SubString(s string, pos, length int) string {
 	}
 	return string(runes[pos:l])
 }
+
+func BlurRange(s string, keep int) string {
+	r := []rune(s)
+	lenR := len(r)
+	if lenR >= 4 && lenR > keep*2 { // 4位以上
+		for i, _ := range r {
+			if i >= keep && i < lenR-keep { // 前后i个
+				r[i] = '*'
+			}
+		}
+	} else {
+		r[0] = '*'
+	}
+
+	return string(r)
+}
+
+func BlurNickname(udId string, nickname string) (str string) {
+	if nickname != "" {
+		str = nickname
+	} else {
+		lenUdId := len(udId)
+		if lenUdId > 8 {
+			str = udId[:4] + udId[lenUdId-4:]
+		} else {
+			str = udId
+		}
+	}
+	str = BlurRange(str, 2)
+	return
+}

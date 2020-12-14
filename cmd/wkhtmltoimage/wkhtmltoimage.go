08b45839a850031f31cdef2c09541ffea249b19b
luo hao <990747088@qq.com>
2020-12-14 12:49:41 +0800

    modify

--- cmd/wkhtmltoimage/wkhtmltoimage.go
+++ cmd/wkhtmltoimage/wkhtmltoimage.go
@@ -3,19 +3,41 @@ package wkhtmltoimage
 import (
 	"github.com/haozzzzzzzz/go-rapid-development/cmd"
 	"github.com/sirupsen/logrus"
+	"github.com/xiam/to"
 )
 
-// 输入html文件地址，生成图片文件
+type Params struct {
+	InputUri   string `json:"input_uri"`
+	OutputPath string `json:"output_path"`
+	Format     string `json:"format"`
+	Width      int    `json:"width"`
+	Height     int    `json:"height"`
+}
 
-func WkHtmlToImage(
-	inputPath string,
-	outputPath string,
-	format string,
-) (err error) {
-	_, err = cmd.RunCommand("./", "wkhtmltoimage", "--format", format, inputPath, outputPath)
+// 输入html uri，生成图片文件
+func WkHtmlToImage(params *Params) (err error) {
+	args := make([]string, 0)
+	args = append(args, "--format", params.Format)
+
+	if params.Width > 0 {
+		args = append(args, "--width", to.String(params.Width))
+	}
+
+	if params.Height > 0 {
+		args = append(args, "--height", to.String(params.Height))
+	}
+
+	// final
+	args = append(args, params.InputUri, params.OutputPath)
+	_, err = cmd.RunCommand(
+		"./",
+		"wkhtmltoimage",
+		args...,
+	)
 	if err != nil {
 		logrus.Errorf("run cmd wkhtmtoimage to generate picture failed. error: %s", err)
 		return
 	}
+
 	return
 }
--- cmd/wkhtmltoimage/wkhtmltoimage_test.go
+++ cmd/wkhtmltoimage/wkhtmltoimage_test.go
@@ -6,7 +6,14 @@ import (
 )
 
 func TestWkHtmlToImage(t *testing.T) {
-	err := WkHtmlToImage("/home/hao/Projects/tool/wkhtmltoimage/sample.html", "./sample.png", "png")
+	//err := WkHtmlToImage("/home/hao/Projects/tool/wkhtmltoimage/sample.html", "./sample.png", "png")
+	err := WkHtmlToImage(&Params{
+		InputUri:   "http://api.videobuddy.vid007.com",
+		OutputPath: "./sample.png",
+		Format:     "png",
+		Width:      0,
+		Height:     0,
+	})
 	if err != nil {
 		t.Error(err)
 		return

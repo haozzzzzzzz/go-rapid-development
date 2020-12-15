4e3c1b9f018b5451be4a0fa88c20762b862c34d7
luo hao <990747088@qq.com>
2020-12-15 13:38:56 +0800

    finish wkhtmltoimage

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"os/exec"
 	"syscall"
 
@@ -8,8 +9,45 @@ import (
 )
 
 // 运行命令
-func RunCommand(workingDir string, name string, args ...string) (exit int, err error) {
+func RunCommand(
+	workingDir string,
+	name string,
+	args ...string,
+) (exit int, err error) {
 	execCommand := exec.Command(name, args...)
+	execCommand.Dir = workingDir
+	output, err := execCommand.Output()
+	strOutput := string(output)
+	if strOutput != "" {
+		logrus.Info(strOutput)
+	}
+
+	if nil != err {
+		logrus.Errorf("run `%s %s`failed.", name, args)
+		exitError, ok := err.(*exec.ExitError)
+		if ok {
+			err = exitError
+			logrus.Error(string(exitError.Stderr))
+			waitStatus := exitError.Sys().(syscall.WaitStatus)
+			exit = waitStatus.ExitStatus()
+		}
+		return
+
+	} else {
+		waitStatus := execCommand.ProcessState.Sys().(syscall.WaitStatus)
+		exit = waitStatus.ExitStatus()
+	}
+
+	return
+}
+
+func RunCommandCtx(
+	ctx context.Context,
+	workingDir string,
+	name string,
+	args ...string,
+) (exit int, err error) {
+	execCommand := exec.CommandContext(ctx, name, args...)
 	execCommand.Dir = workingDir
 	output, err := execCommand.Output()
 	strOutput := string(output)
--- cmd/wkhtmltoimage/wkhtmltoimage.go
+++ cmd/wkhtmltoimage/wkhtmltoimage.go
@@ -1,35 +1,82 @@
+// 这个工具有个劣势：对于html、css的特性支持不足
 package wkhtmltoimage
 
 import (
+	"context"
 	"github.com/haozzzzzzzz/go-rapid-development/cmd"
 	"github.com/sirupsen/logrus"
 	"github.com/xiam/to"
 )
 
+type LoadErrorHandleType string
+
+const (
+	LoadErrorHandleAbort  LoadErrorHandleType = "abort"
+	LoadErrorHandleIgnore LoadErrorHandleType = "ignore"
+	LoadErrorHandleSkip   LoadErrorHandleType = "skip"
+)
+
+type LoadMediaErrorHandleType string
+
+const (
+	LoadMediaErrorHandleAbort  LoadMediaErrorHandleType = "abort"
+	LoadMediaErrorHandleIgnore LoadMediaErrorHandleType = "ignore"
+	LoadMediaErrorHandlerSkip  LoadMediaErrorHandleType = "skip"
+)
+
+type LogLevelType string
+
+const (
+	LogLevelTypeNone  LogLevelType = "none"
+	LogLevelTypeError LogLevelType = "error"
+	LogLevelTypeWarn  LogLevelType = "warn"
+	LogLevelTypeInfo  LogLevelType = "info"
+)
+
+// man wkhtmltoimage
 type Params struct {
-	WorkingDir string `json:"working_dir"`
-	ExecName   string `json:"exec_name"`
-	InputUri   string `json:"input_uri"`
-	OutputPath string `json:"output_path"`
-	Format     string `json:"format"`
-	Width      int    `json:"width"`
-	Height     int    `json:"height"`
+	WorkingDir             string                   `json:"working_dir"`
+	ExecName               string                   `json:"exec_name"`
+	InputUri               string                   `json:"input_uri"`
+	OutputPath             string                   `json:"output_path"`
+	Format                 string                   `json:"format"`
+	Width                  int                      `json:"width"`  // 像素宽度。
+	Height                 int                      `json:"height"` // 像素高度。
+	NoDebugJavascript      bool                     `json:"no_debug_javascript"`
+	DisableJavascript      bool                     `json:"disable_javascript"`
+	Quality                int                      `json:"quality"`                   // 图片质量。0～100
+	EnableLocalFileAccess  bool                     `json:"enable_local_file_access"`  // 允许访问本地文件
+	LoadErrorHandling      LoadErrorHandleType      `json:"load_error_handling"`       // 页面加载失败处理。
+	LoadMediaErrorHandling LoadMediaErrorHandleType `json:"load_media_error_handling"` // 媒体加载失败处理
+	LogLevel               LogLevelType             `json:"log_level"`
+	Allow                  string                   `json:"allow"` // 允许程序加载的本地目录下的文件
 }
 
 func (m *Params) Logger() *logrus.Entry {
 	return logrus.WithFields(logrus.Fields{
-		"working_dir": m.WorkingDir,
-		"exec_name":   m.ExecName,
-		"input_uri":   m.InputUri,
-		"output_path": m.OutputPath,
-		"format":      m.Format,
-		"width":       m.Width,
-		"height":      m.Height,
+		"working_dir":               m.WorkingDir,
+		"exec_name":                 m.ExecName,
+		"input_uri":                 m.InputUri,
+		"output_path":               m.OutputPath,
+		"format":                    m.Format,
+		"width":                     m.Width,
+		"height":                    m.Height,
+		"no_debug_javascript":       m.NoDebugJavascript,
+		"disable_javascript":        m.DisableJavascript,
+		"quality":                   m.Quality,
+		"enable_local_file_access":  m.EnableLocalFileAccess,
+		"load_error_handling":       m.LoadErrorHandling,
+		"load_media_error_handling": m.LoadMediaErrorHandling,
+		"log_level":                 m.LogLevel,
+		"allow":                     m.Allow,
 	})
 }
 
 // 输入html uri，生成图片文件
-func WkHtmlToImage(params *Params) (err error) {
+func WkHtmlToImage(
+	ctx context.Context,
+	params *Params,
+) (err error) {
 	args := make([]string, 0)
 	args = append(args, "--format", params.Format)
 
@@ -39,6 +86,38 @@ func WkHtmlToImage(params *Params) (err error) {
 
 	if params.Height > 0 {
 		args = append(args, "--height", to.String(params.Height))
+	}
+
+	if params.NoDebugJavascript {
+		args = append(args, "--no-debug-javascript")
+	}
+
+	if params.DisableJavascript {
+		args = append(args, "--disable-javascript")
+	}
+
+	if params.Quality > 0 {
+		args = append(args, "--quality", to.String(params.Quality))
+	}
+
+	if params.EnableLocalFileAccess {
+		args = append(args, "--enable-local-file-access")
+	}
+
+	if params.LoadErrorHandling != "" {
+		args = append(args, "--load-error-handling", string(params.LoadErrorHandling))
+	}
+
+	if params.LoadMediaErrorHandling != "" {
+		args = append(args, "--load-media-error-handling", string(params.LoadMediaErrorHandling))
+	}
+
+	if params.LogLevel != "" {
+		args = append(args, "--log-level", to.String(params.LogLevel))
+	}
+
+	if params.Allow != "" {
+		args = append(args, "--allow", params.Allow)
 	}
 
 	// final
@@ -53,9 +132,10 @@ func WkHtmlToImage(params *Params) (err error) {
 
 	logger := params.Logger()
 
-	_, err = cmd.RunCommand(
-		"./",
-		"wkhtmltoimage",
+	_, err = cmd.RunCommandCtx(
+		ctx,
+		params.WorkingDir,
+		params.ExecName,
 		args...,
 	)
 	if err != nil {

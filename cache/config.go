568e2911915577a01a740469205ad37debd0e96f
luo hao <990747088@qq.com>
2021-03-11 10:36:28 +0800

    add config

--- cache/config.go
+++ cache/config.go
@@ -7,6 +7,7 @@ import (
 
 type RedisConfigFormat struct {
 	Address  string `json:"address" yaml:"address" validate:"required"`
+	Password string `json:"password" yaml:"password"`
 	PoolSize int    `json:"pool_size" yaml:"pool_size"` // 默认是CPU核心数*10
 	DB       int    `json:"db" yaml:"db"`
 }
@@ -14,10 +15,29 @@ type RedisConfigFormat struct {
 func NewRedisClient(config *RedisConfigFormat) (client *redis.Client, err error) {
 	client = redis.NewClient(&redis.Options{
 		Addr:     config.Address,
+		Password: config.Password,
 		PoolSize: config.PoolSize,
 		DB:       config.DB,
 	})
 
+	result, err := client.Ping().Result()
+	if nil != err {
+		logrus.Errorf("ping redis service failed. result: %s, err:%s.", result, err)
+		return
+	}
+
+	return
+}
+
+// redis://<user>:<pass>@localhost:6379/<db>
+func NewRedisClientByUrl(redisUrl string) (client *redis.Client, err error) {
+	options, err := redis.ParseURL(redisUrl)
+	if err != nil {
+		logrus.Errorf("parse redis url failed. error: %s", err)
+		return
+	}
+
+	client = redis.NewClient(options)
 	result, err := client.Ping().Result()
 	if nil != err {
 		logrus.Errorf("ping redis service failed. result: %s, err:%s.", result, err)
